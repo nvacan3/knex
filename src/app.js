2a0464a1bed49512036d67f37b1902e706c31403
nvacan3 <nicolas.b.moise@gmail.com>
2023-05-26 16:22:25 -0400

    Completes a good day of work

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 8081;
+
+const knex = require("knex")(
+  require("../knexfile.js")[process.env.NODE_ENV || "development"]
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Application up and running");
+});
+
+app.get("/movies", (req, res) => {
+  knex
+    .select("*")
+    .from("movies")
+    .then((data) => res.status(200).json(data))
+    .catch((err) =>
+      res.status(404).json({
+        message:
+          "The data you are looking for could not be found. Please try again",
+      })
+    );
+});
+
+app.listen(port, () => {
+  console.log(`The server is running on ${port}`);
+});
